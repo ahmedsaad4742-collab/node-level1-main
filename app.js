321cf40b464f493c9eacac5ec62eae69c044c959
ahmedsaad4742-collab <ahmedsaad@gmail.com>
2026-06-07 01:01:42 +0300

    lesone3

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
+const express =require('express')
 const app = express()
-const port = 30001
+const port = 3001
+const mongoose = require('mongoose')
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname})
-})
+mongoose
+.connect
+("mongodb://ahmedsaad4742_db_user:idk6FXklrrlTNFS3@ac-kfr5rvo-shard-00-00.gkfdtrh.mongodb.net:27017,ac-kfr5rvo-shard-00-01.gkfdtrh.mongodb.net:27017,ac-kfr5rvo-shard-00-02.gkfdtrh.mongodb.net:27017/?ssl=true&replicaSet=atlas-w8pnh2-shard-0&authSource=admin&appName=Cluster0")
+.then(() => {
+  console.log(`Http://localhost:${port}/`);
+  app.get('/', (req, res) => {
+    res.render("./views/home.html", {root: __dirname}) 
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+  })
 })
+.catch((err) => {console.log(err)
+});
